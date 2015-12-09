5456593de712e6ad0dfe1fe22ec710128c7df557
Sarah Elizabeth Olson <solson3@remote205.helios.nd.edu>
2015-12-09 18:13:46 -0500

    added lobster and fish

--- final.c
+++ final.c
@@ -11,6 +11,7 @@ void createSun(double *ySun);
 void createDuckHead(double *x1, double *y1, double *beakMovement);
 void createBird(double *bX, double *bY, double *i, double *l, double velocity, double *doRotate);
 void createFish(double *fX, double *fY);
+void createLobster(double *lX, double *lY);
 
 int main()
 {
@@ -25,9 +26,11 @@ int main()
         double x1=50; //Where the duck starts (on left hand of page)
         double y1=120; 
         double bX=400;//the starting point for the bird
-	double bY=50;//the starting point for the bird
-	double fX=10;
+	double bY=50;
+	double fX=10; //starting point for the fish
 	double fY=400;
+	double lX=0; //starting point for the lobster
+	double lY=600;
 	double doRotate=1;
 	double ySun=50; //the sun is always setting/rising
 	int velocity=5; //basic velocity of the bird in the air
@@ -39,6 +42,7 @@ int main()
 		createDuckHead(&x1, &y1, &beakMovement);
 		createBird(&bX, &bY, &i, &l, velocity, &doRotate);
 		createFish(&fX, &fY);
+		createLobster(&lX, &lY);
                 gfx_flush();
 
                 //Check if the user has decided to quit the program
@@ -85,6 +89,16 @@ void createLayout(){
 	gfx_line(26, 680, 26, 750);
 	gfx_line(17, 760, 15, 700);
 	gfx_line(34, 760, 38, 700);
+
+	gfx_line(140, 740, 140, 780);
+	gfx_line(150, 720, 150, 760);
+	gfx_line(145, 710, 145, 770);
+	gfx_line(155, 726, 158, 750);
+	gfx_line(165, 715, 165, 770);
+	gfx_line(156, 720, 156, 750);
+	gfx_line(167, 790, 170, 720);
+	gfx_line(154, 780, 158, 740);
+
 
 	gfx_line(720, 700, 720, 780);
 	gfx_line(730, 680, 730, 760);
@@ -173,4 +187,46 @@ void createFish(double *fX, double *fY){
 	gfx_line((int)*fX-40, (int)*fY+20, (int)*fX-20, (int)*fY+20);
 	gfx_line((int)*fX-30, (int)*fY+7, (int)*fX-10, (int)*fY+7);
 	gfx_line((int)*fX, (int)*fY+20, (int)*fX, (int)*fY+20);
+}
+
+void createLobster(double *lX, double *lY){
+	if (*lX<=280){
+		*lX=*lX+.05;
+		*lY=*lY+.03;
+	}
+	if (*lX<=500 && *lX>=280){
+		*lX=*lX+.05;
+	}
+	if (*lX>=500){
+		*lX=*lX+.05;
+		*lY=*lY-.03;
+	}
+	if (*lX>=795){
+		*lX=0;
+	}
+	gfx_color(255, 4, 4);
+	//Eye
+	gfx_fill_circle((int)*lX, (int)*lY, 8);
+	gfx_fill_circle((int)*lX+2, (int)*lY-10, 6);
+	gfx_fill_circle((int)*lX-2, (int)*lY-10, 6);
+	//Crab hook
+	gfx_line((int)*lX+8, (int)*lY-2, (int)*lX+14, (int)*lY-10);
+	gfx_line((int)*lX+14, (int)*lY-10, (int)*lX+18, (int)*lY-20);
+	gfx_line((int)*lX+14, (int)*lY-10, (int)*lX+11, (int)*lY-18);
+	gfx_line((int)*lX-8, (int)*lY-2, (int)*lX-14, (int)*lY-10);
+	gfx_line((int)*lX-14, (int)*lY-10, (int)*lX-18, (int)*lY-20);
+	gfx_line((int)*lX-14, (int)*lY-10, (int)*lX-11, (int)*lY-18);
+	//Leg additions
+	gfx_line((int)*lX+5, (int)*lY+2, (int)*lX+16, (int)*lY+1);
+	gfx_line((int)*lX+5, (int)*lY+3, (int)*lX+16, (int)*lY+3);
+	gfx_line((int)*lX+5, (int)*lY+4, (int)*lX+16, (int)*lY+5);
+	gfx_line((int)*lX-5, (int)*lY+2, (int)*lX-16, (int)*lY+1);
+	gfx_line((int)*lX-5, (int)*lY+3, (int)*lX-16, (int)*lY+3);
+	gfx_line((int)*lX-5, (int)*lY+4, (int)*lX-16, (int)*lY+5);
+	//Pupil of the eye
+	gfx_color(0,0,0); 
+	gfx_fill_circle((int)*lX+2, (int)*lY-10, 2);
+	gfx_fill_circle((int)*lX-2, (int)*lY-10, 2);
+	//Lips of lobster
+	gfx_line((int)*lX-3, (int)*lY+2, (int)*lX+3, (int)*lY+2);
 }
